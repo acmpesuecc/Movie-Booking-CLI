0ebb906053ea91d7647b4aeb63f10fc9be57a404
Ayush-jk <Payush3103@gmail.com>
2024-10-19 02:56:38 +0530

    added tables and modified main.c

--- src/main.c
+++ src/main.c
@@ -2,8 +2,9 @@
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
+#include "sqlite3.h"
+#include <windows.h>
 
-#define MAX_PATH 260
 typedef struct {
     char name[50];
     char email[50];
